9698b2806532282cca78f022ee091321428f204b
Jonathan Hernández <jona73eli@gmail.com>
2026-07-01 09:45:52 -0600

    feat: agregar enums de estado y prioridad

---
+++ src/models/Priority.ts
@@ -0,0 +1,6 @@
+
+export enum Priority {
+    Baja = "baja",
+    Media = "media",
+    Alta = "alta",
+}
---
+++ src/models/Status.ts
@@ -0,0 +1,6 @@
+
+export enum Status {
+    Atrasado = "atrasado",
+    Pendiente = "pendiente",
+    Completado = "completado",
+}
