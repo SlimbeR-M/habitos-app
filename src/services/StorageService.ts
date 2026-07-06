e5015d3247ffc75479d8f4cfd5b2ba7e6050eda5
Jonathan Hernández <jona73eli@gmail.com>
2026-07-06 14:17:52 -0600

    feat: agregar Singleton StorageService con persistencia en localStorage

---
+++ src/services/StorageService.ts
@@ -0,0 +1,28 @@
+
+export class StorageService {
+    private static instancia: StorageService;
+
+    private constructor(){}
+
+    static obtenerInstancia(): StorageService {
+        if(!StorageService.instancia) {
+            StorageService.instancia = new StorageService();
+        }
+        return StorageService.instancia;
+    }
+
+    guardar(clave: string, datos: any): void {
+        const info = JSON.stringify(datos);
+        localStorage.setItem(clave, info);
+    }
+
+    obtener(clave: string): any | null  {
+        let datosGuardados = localStorage.getItem(clave);
+        if(datosGuardados === null){
+            console.log("No hay datos guardados con esa clave");
+            return null;
+        } else {
+            return JSON.parse(datosGuardados);
+        }
+    }
+}
