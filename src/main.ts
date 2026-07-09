6e1f7885ed3b704918b58cc38ec0c9e81a90abc7
Jonathan Hernández <jona73eli@gmail.com>
2026-07-09 13:28:29 -0600

    feat: conectar StorageService y Repository en main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import './style.css'
+import { StorageService } from './services/StorageService'
+import { Repository } from './services/Repository'
+import { Habit } from './models/Habit'
+import { OneTimeTask } from './models/OneTimeTask';
+import { Priority } from './models/Priority'
+import { Status } from './models/Status';
 
-console.log("habitos app")
+const storage = StorageService.obtenerInstancia();
+
+const habitos = new Repository<Habit>(storage,"habitos");
+const tareas = new Repository<OneTimeTask>(storage,"tareas");
+
 
