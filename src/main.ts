20a903300c397e999ae7974105b5ed5e605a4558
Jonathan Hernández <jona73eli@gmail.com>
2026-07-14 13:17:41 -0600

    feat: conectar formulario con creacionn de habitos

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import './style.css'
 import { StorageService } from './services/StorageService'
 import { Repository } from './services/Repository'
-import { Habit } from './models/Habit'
+import { Habit } from './models/Habit';
 import { OneTimeTask } from './models/OneTimeTask';
 import { Priority } from './models/Priority'
 import { Status } from './models/Status';
 
-const storage = StorageService.obtenerInstancia();
+const storage = StorageService.obtenerInstancia(),
+    habitos = new Repository<Habit>(storage,"habitos"),
+    tareas = new Repository<OneTimeTask>(storage,"tareas");
 
-const habitos = new Repository<Habit>(storage,"habitos");
-const tareas = new Repository<OneTimeTask>(storage,"tareas");
+const formulario = document.querySelector("#form-habito") as HTMLFormElement,
+    inpNombre = document.querySelector("#nombre-habito") as HTMLInputElement,
+    txtDescripcion = document.querySelector("#descripcion") as HTMLTextAreaElement,
+    slctPrioridad = document.querySelector("#prioridad") as HTMLSelectElement;
 
+formulario.addEventListener("submit", (event)=> {
+    event.preventDefault();
+
+    const nombre = inpNombre.value,
+        descripcion = txtDescripcion.value,
+        prioridad = slctPrioridad.value as Priority,
+        id = Date.now();
+
+    const habito = new Habit(id, nombre, prioridad, descripcion); 
+    habitos.agregar(habito);
+    formulario.reset();
+});
 
